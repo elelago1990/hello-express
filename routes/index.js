600a00e2da1e0857569aae926fe66c1160bb9e4a
afd <afd@alup5.academiapostal.es.local>
2020-01-28 20:55:38 +0100

    fgfgfdgfgfg

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,15 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+  const products= [
+    {name:"microondas", price: 45 , stock:6},
+    {name:"rigorifico", price: 200, stock:4},
+    {name:"tv", price: 120, stock: 2},
+    {name:"impresora", price: 200, stock:6},
+    {name:"roomba", price: 99, stock: 12}
+  ];
+  res.render('index', { title: 'Elena', products });
 });
 
 module.exports = router;
