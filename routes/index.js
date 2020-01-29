72dc2afadd6fd620e12bcc48fabfef18736eefa4
afd <afd@alup5.academiapostal.es.local>
2020-01-29 18:30:28 +0100

    cambios imaxes

--- routes/index.js
+++ routes/index.js
@@ -5,11 +5,11 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
 
   const products= [
-    {name:"microondas", price: 45 , stock:6},
-    {name:"rigorifico", price: 200, stock:4},
-    {name:"tv", price: 120, stock: 2},
-    {name:"impresora", price: 200, stock:6},
-    {name:"roomba", price: 99, stock: 12}
+    {name:"microondas", price: 45 , stock:6, imaxes: "microond.jpg"},
+    {name:"rigorifico", price: 200, stock:4, imaxes: "frigorifico.jpg"},
+    {name:"tv", price: 120, stock: 2, imaxes: "tv.jpeg"},
+    {name:"impresora", price: 200, stock:6, imaxes:"impre_.jpg"},
+    {name:"roomba", price: 99, stock: 8, imaxes:  "roombat.jpg"},
   ];
   res.render('index', { title: 'Elena', products });
 });
