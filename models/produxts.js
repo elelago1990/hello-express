77a0955d4d0b664ecca1140370d54880b48f01e9
afd <afd@alup5.academiapostal.es.local>
2020-01-30 18:15:58 +0100

    nuevos cambios 2

---
+++ models/produxts.js
@@ -0,0 +1,9 @@
+module.exports=[
+    {name:"microondas", price: 45 , stock:6, imaxes: "microond.jpg", referencia: "555mi"},
+    {name:"rigorifico", price: 200, stock:4, imaxes: "frigorifico.jpg",referencia: "56mi"},
+    {name:"tv", price: 120, stock: 0, imaxes: "tv.jpeg", referencia: "575mi"},
+    {name:"impresora", price: 200, stock:6, imaxes:"impre_.jpg", referencia: "595mi"},
+    {name:"roomba", price: 99, stock: 8, imaxes:  "roombat.jpg", referencia: "545mi"},
+
+
+];
--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,25 @@
 var express = require('express');
 var router = express.Router();
-
+var products= require( "../models/produxts.js");
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
-  const products= [
-    {name:"microondas", price: 45 , stock:6, imaxes: "microond.jpg"},
-    {name:"rigorifico", price: 200, stock:4, imaxes: "frigorifico.jpg"},
-    {name:"tv", price: 120, stock: 2, imaxes: "tv.jpeg"},
-    {name:"impresora", price: 200, stock:6, imaxes:"impre_.jpg"},
-    {name:"roomba", price: 99, stock: 8, imaxes:  "roombat.jpg"},
-  ];
+ 
   res.render('index', { title: 'Elena', products });
+});
+
+router.get("/products/:ref",  function (req, res, next){
+  var ref=req.params.ref;
+
+  const product=products.find(function(p){
+    return p.referencia==ref;
+  });
+
+  if (product){
+  res.render("referencia", {product});}
+  else{
+    res.redirect("/error");
+  }
 });
 
 module.exports = router;
